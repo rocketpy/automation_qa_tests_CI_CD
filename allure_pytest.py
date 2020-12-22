acf6403e4829b93d0ac3d63b1533c0eab88ee9dc
rocketpy <dimitr.ushakov@gmail.com>
2020-12-22 18:54:13 +0200

    updated

--- allure_pytest.py
+++ allure_pytest.py
@@ -1,1 +1,11 @@
+#  Allure Framework is a flexible lightweight multi-language test report tool
 
+#  Allure pytest integration
+#  Docs:  https://docs.qameta.io/allure/
+#  PyPi:  https://pypi.org/project/allure-pytest/
+
+#  pip install allure-pytest
+
+#  Usage
+#  py.test --alluredir=%allure_result_folder% ./tests
+#  allure serve %allure_result_folder%
