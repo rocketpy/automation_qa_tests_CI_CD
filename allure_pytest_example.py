a3ad382a0effe17c20f9b8aa2462cb6abe62d54b
rocketpy <dimitr.ushakov@gmail.com>
2020-12-23 23:16:58 +0200

    added imports

--- allure_pytest_example.py
+++ allure_pytest_example.py
@@ -1,1 +1,10 @@
+import time
+import pytest
+import allure
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+
+
 
