80626500805f4d576f1b1b9e1d7c711075685388
rocketpy <dimitr.ushakov@gmail.com>
2021-01-22 22:43:32 +0200

    Update solved_problems_Allure.py

--- solved_problems_Allure.py
+++ solved_problems_Allure.py
@@ -1,1 +1,17 @@
+#  A list with examples of solved problems
+# https://automated-testing.info/t/allure-podborka-tipichnyh-problem-voznikayushhih-v-proczesse-ispolzovaniya-i-ih-resheniya/20759
+
+
+#  How creating a HTML report
+"""
+run Allure with command:  py.test EW.py --alluredir directory-with-results
+
+В директории allure-commandline/bin/ выполняете allure generate directory-with-results/
+Отчет в виде index.html сгенерится в allure-commandline/bin/allure-report/
+Если же хочется поместить этот index.html куда-то в другое место, то выполняем
+allure generate directory-with-results/ -o directory-where-u-want-indexhtmlreport-to-be-generated/
+
+Нужно очищать папку allure-results
+
+"""
 
