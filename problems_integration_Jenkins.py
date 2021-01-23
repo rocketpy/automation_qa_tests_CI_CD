59a773ec82bcc665cd0154a24bf99e9dbf942f5f
rocketpy <dimitr.ushakov@gmail.com>
2021-01-23 23:37:53 +0200

    Update problems_integration_Jenkins.py

--- problems_integration_Jenkins.py
+++ problems_integration_Jenkins.py
@@ -1,1 +1,18 @@
+# install the module first:
 
+#  pip install -U pytest
+
+# create a virtualenv and execute your script inside in isolation.
+
+#!/bin/bash
+
+export WORKSPACE=`pwd`
+
+# Create/Activate virtualenv
+virtualenv testenv -p /path/to/python/bin
+source testenv/bin/activate
+
+pip install -U pytest 
+
+#  run script here
+....
