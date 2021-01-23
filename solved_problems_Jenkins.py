0bb5ade14430f553f1f766a21681d80eeabf3f43
rocketpy <dimitr.ushakov@gmail.com>
2021-01-23 22:39:44 +0200

    Update solved_problems_Jenkins.py

--- solved_problems_Jenkins.py
+++ solved_problems_Jenkins.py
@@ -1,1 +1,12 @@
+#  How  to run regresion tests with pytest connection to Git
 
+#  use : 'Pyenv Pipeline' plugin (https://plugins.jenkins.io/pyenv-pipeline)
+"""
+stage("test PythonEnv") {
+
+    withPythonEnv('python3') {
+        sh 'pip install pytest'
+        sh 'pytest mytest.py'
+    }
+}
+"""
