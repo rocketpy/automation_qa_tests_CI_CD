57179725935b3ab8b66a7e38411c8361c8e7e102
rocketpy <dimitr.ushakov@gmail.com>
2021-08-16 21:52:36 +0300

    Update project_1.py

--- New project/project_1.py
+++ New project/project_1.py
@@ -1,1 +1,11 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture()
+def test_setup():
+    global driver
+    driver = webdriver.Chrome(executable_path='C:\\...')  # Firefox
+    driver.implicitly_wait(10)
+    driver.maximize_window()
 
