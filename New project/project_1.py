398354566efc1fb72c20333456358b0545355164
rocketpy <dimitr.ushakov@gmail.com>
2021-08-17 23:06:16 +0300

    Update project_1.py

--- New project/project_1.py
+++ New project/project_1.py
@@ -8,4 +8,11 @@ def test_setup():
     driver = webdriver.Chrome(executable_path='C:\\...')  # Firefox
     driver.implicitly_wait(10)
     driver.maximize_window()
+    yield
+    driver.quit()
+    
 
+@allure.description("Validating a login form")
+@allure.severity(severity_level=CRITICAL)
+def test_valid_login(test_setup):
+    driver.get("https://")
